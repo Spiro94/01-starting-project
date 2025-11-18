e83acf692724028e7a7529ff114229cac4e61bd8
Spiro94 <danieljosevillamizar@gmail.com>
2025-11-18 11:57:18 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
